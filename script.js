2fb6c41ca13a4520ca302ac205881515954b4c97
binish rizwan <binishrizwan002@gmail.com>
2024-10-06 16:46:38 +0500

    start  working in js file

---
+++ script.js
@@ -0,0 +1,10 @@
+var w = document.getElementById("weight").value
+var h = document.getElementById("height").value
+var b = document.getElementById("button").value
+
+function bmicalculator(){
+
+    
+
+}
+
