14ad4b0d3cb1aa2d82dd920b2d3b4ff4b0b45102
binish rizwan <binishrizwan002@gmail.com>
2024-10-13 15:38:34 +0500

    done

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-function bmicalculator = {
 
 
 
-var w = document.getElementById("weight").value
-var h = document.getElementById("height").value
-var b = document.getElementById("button").value
 
-var bmi = w / h * h;
+
+function bmicalculator(){
+
+    var w =  document.getElementById("weight").value
+
+    var h = document.getElementById("height").value 
+
+    // var b = document.getElementById("button")
+    var r = document.getElementById("result").value
+
+
+
+
+    if(input1 === "")
+
+    // var bmi = w / (h * h);
+    // r.textcontent = "your BMI is" + bmi.toFixed(2)
 }
+    
+
+
+
+
+
 
 
 
